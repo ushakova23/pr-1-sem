4f46fb2dd3fa620eb84dbf357eac972f298f29b5
ushakova23 <113995972+ushakova23@users.noreply.github.com>
2022-10-18 15:50:41 +0300

    Add files via upload

---
+++ 1744C.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cmath>
+int main()
+{
+    int t=0;//if (c==g) cout<<0;
+    std::cin>>t;
+    while (t--)
+    {
+        std::string s =" ";
+        char c;
+        int n=0;
+        int k=0;//количество секунд
+        std::cin>>n>>c>>s;
+    }
+   
+    
+    
+ return 0;   
+}
