57a08e5e2cfbca1928f86b6866716125d7772974
ushakova23 <113995972+ushakova23@users.noreply.github.com>
2022-12-18 14:33:14 +0300

    Add files via upload

---
+++ 1690D.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+ 
+int main() 
+{
+	int t;
+    int n;
+    int k;
+    int y; 
+    int m;
+	std::cin >> t;
+	for (int j = 0; j < t; j++) 
+	{
+		std::cin >> n >> k;
+		std::vector<char> a(n);
+		std::vector<int> b(n + 1);
+		y = 0;
+		for (int i = 0; i < n; i++) 
+		{
+			std::cin >> a[i];
+			if (a[i] == 'B') y++;
+			b[i + 1] = y;
+		}
+		m = 0;
+		for (int i = 1; i <= n - k + 1; i++) 
+		{
+			m = std::max(m, b[i + k - 1] - b[i - 1]);
+		}
+		std::cout << k - m << std::endl;
+	}
+	return 0;
+}
